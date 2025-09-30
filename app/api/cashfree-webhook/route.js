37dfbe426176b52fbec568b54f1d8fb4ea50f9de
Anshu Kumar <110124018@nitt.edu>
2025-09-30 22:46:32 +0530

    supabase integrated

--- app/api/cashfree-webhook/route.js
+++ app/api/cashfree-webhook/route.js
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import crypto from "crypto";
 import mongoose from "mongoose";
 import PaymentModel from "@/app/Schema/paymentModel";
+import { supabaseAdmin } from "../../lib/supabaseServer";
 export async function POST(request) {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
@@ -27,33 +28,45 @@ export async function POST(request) {
 console.log("Signature verified high five! ✋");
     const data = JSON.parse(rawBody);
     console.log("✅ Webhook received:", data);
+
+    const { error: Err } = await supabaseAdmin
+      .from("payments")
+      .update({ status: data.data.payment.payment_status,
+        transaction_id: data.data.payment.cf_payment_id,
+        })
+      .eq("team_id", "131825");
+
+    if (Err) {
+      console.error("Supabase update payment error:", Err);
+      return NextResponse.json({ ok: false, error: Err.message }, { status: 500 });
+    }
     
 
     // Process the webhook data
-    const  payment= await PaymentModel.findById(data.data.order.order_id);
-    if (payment) {
-      payment.status = data.data.payment.payment_status;
-      payment.cf_payment_id = data.data.payment.cf_payment_id;
-      payment.updated_at = new Date();
-      await payment.save();
-      console.log("Payment status updated:", payment);
-    } else {
+    // const  payment= await PaymentModel.findById(data.data.order.order_id);
+    // if (payment) {
+    //   payment.status = data.data.payment.payment_status;
+    //   payment.cf_payment_id = data.data.payment.cf_payment_id;
+    //   payment.updated_at = new Date();
+    //   await payment.save();
+    //   console.log("Payment status updated:", payment);
+    // } else {
             
-      const newPayment = new PaymentModel({
-        _id: data.order.data.order_id,
-        amount: data.data.order.order_amount,
-        currency: data.data.order.order_currency,
-        status: data.data.payment.payment_status,
-        name: data.data.order.customer_details.customer_name,
-        created_at: new Date(data.data.order.created_at),
-        updated_at: new Date(),
-        order_id: data.data.order.order_id,
-        payment_session_id: data.data.order.payment_session_id,
-        cf_payment_id: data.data.order.cf_payment_id,
-      });
-      await newPayment.save();
-      console.log("New payment record created:", newPayment);
-    }
+    //   const newPayment = new PaymentModel({
+    //     _id: data.order.data.order_id,
+    //     amount: data.data.order.order_amount,
+    //     currency: data.data.order.order_currency,
+    //     status: data.data.payment.payment_status,
+    //     name: data.data.order.customer_details.customer_name,
+    //     created_at: new Date(data.data.order.created_at),
+    //     updated_at: new Date(),
+    //     order_id: data.data.order.order_id,
+    //     payment_session_id: data.data.order.payment_session_id,
+    //     cf_payment_id: data.data.order.cf_payment_id,
+    //   });
+    //   await newPayment.save();
+    //   console.log("New payment record created:", newPayment);
+    // }
     
     
     // Always respond 200 OK
--- app/api/checkout/route.js
+++ app/api/checkout/route.js
@@ -3,10 +3,12 @@ import { NextResponse } from "next/server";
 import mongoose from "mongoose";
 import { Cashfree, CFEnvironment } from "cashfree-pg";
 import PaymentModel from "@/app/Schema/paymentModel";
+import { supabaseAdmin } from "../../lib/supabaseServer";
 import crypto from "crypto";
+import { create } from "domain";
 
 
-function verifyCashfreeSignature({ cf_order_id ,signature}) {
+function verifyCashfreeSignature({ cf_order_id, signature }) {
   const secretKey = process.env.CASHFREE_SECRET;
   const rawSignature = cf_order_id + secretKey;
   const generatedSignature = crypto.createHash("sha256").update(rawSignature).digest("hex");
@@ -16,7 +18,7 @@ export async function POST(request) {
   await mongoose.connect(process.env.MONGODB_URI);
 
   const body = await request.json();
-console.log(body);
+  console.log(body);
   const cashfree = new Cashfree(
     CFEnvironment.SANDBOX,
     process.env.CASHFREE_ID,
@@ -37,9 +39,9 @@ console.log(body);
       order_meta: {
         return_url:
           "https://b694e83d278d.ngrok-free.app/",
-          notify_url: 
-          "https://b694e83d278d.ngrok-free.app/api/cashfree-webhook",
-         
+        notify_url:
+          "https://dae2d657738c.ngrok-free.app/api/cashfree-webhook",
+
       },
       order_note: "Team registration payment",
     };
@@ -47,22 +49,51 @@ console.log(body);
     const response = await cashfree.PGCreateOrder(orderRequest);
     const order = response.data;
     console.log("Cashfree Order Response:", order);
+    const { error: InsertErr } = await supabaseAdmin
+      .from("payments")
+      .insert({
+        team_id: "131825",
+        expiry_time: new Date(order.order_expiry_time),
+        order_id: order.order_id,
+        payment_session_id: order.payment_session_id,
+        status: "Pending",
+        raw_payload: order,
+        created_at: new Date(),
+        
+      })
+      .select();
 
-    // Save to MongoDB
-    const newPayment = new PaymentModel({
-      _id: order.order_id,
-      amount: order.order_amount,
-      currency: order.order_currency,
-      status: "PENDING",
-      name: body.teamName ?? null,
-      created_at: new Date(),
-      order_id: order.order_id,
-      payment_session_id: order.payment_session_id,
-      user_id: body.userId ?? null,
+    if (InsertErr) {
+      console.error("Supabase insert error:", InsertErr);
+      return NextResponse.json({ ok: false, error: InsertErr.message }, { status: 500 });
+    }
 
-    });
+    // const { error: TeamErr } = await supabaseAdmin
+    //   .from("teams")
+    //   .update({ payment_status: "Pending" })
+    //   .eq("team_id", team_id);
 
-    await newPayment.save();
+    // if (TeamErr) {
+    //   console.error("Supabase update team error:", TeamErr);
+    //   return NextResponse.json({ ok: false, error: TeamErr.message }, { status: 500 });
+    // }
+   
+
+    // // Save to MongoDB
+    // const newPayment = new PaymentModel({
+    //   _id: order.order_id,
+    //   amount: order.order_amount,
+    //   currency: order.order_currency,
+    //   status: "PENDING",
+    //   name: body.teamName ?? null,
+    //   created_at: new Date(),
+    //   order_id: order.order_id,
+    //   payment_session_id: order.payment_session_id,
+    //   user_id: body.userId ?? null,
+
+    // });
+
+    // await newPayment.save();
 
     return NextResponse.json({
       id: order.order_id,
@@ -76,55 +107,55 @@ console.log(body);
     return NextResponse.json({ error: err.message }, { status: 500 });
   }
 }
-export async function PUT(request) {
-  await mongoose.connect(process.env.MONGODB_URI);
+// export async function PUT(request) {
+//   await mongoose.connect(process.env.MONGODB_URI);
 
-  const body = await request.json();
+//   const body = await request.json();
 
-  const cashfree = new Cashfree(
-    CFEnvironment.SANDBOX,
-    process.env.CASHFREE_ID,
-    process.env.CASHFREE_SECRET
-  );
+//   const cashfree = new Cashfree(
+//     CFEnvironment.PRODUCTION,
+//     process.env.CASHFREE_ID,
+//     process.env.CASHFREE_SECRET
+//   );
 
-  try {
-    const order = await cashfree.PGFetchOrder(body.cashfree_order_id);
-
-
-    let payment = await PaymentModel.findById(order.data.order_id);
+//   try {
+//     const order = await cashfree.PGFetchOrder(body.cashfree_order_id);
 
 
-    if (!payment) {
-      payment = new PaymentModel({
-        _id: order.data.order_id,
-        amount: order.data.order_amount,
-        currency: order.data.order_currency,
-        status: order.data.order_status,
-        name: body.teamName ?? null,
-        created_at: new Date(),
-        order_id: order.data.order_id,
-        payment_session_id: order.data.payment_session_id,
-        user_id: body.userId ?? null,
-      });
-    }
-    else {
-      payment.status = order.data.order_status;
-      payment.amount = order.data.order_amount;
-      payment.currency = order.data.order_currency;
-      payment.updated_at = new Date();
-    }
-    
+//     let payment = await PaymentModel.findById(order.data.order_id);
 
-    await payment.save();
 
-    return NextResponse.json({
-      id: order.data.order_id,
-      amount: order.data.order_amount,
-      currency: order.data.order_currency,
-      status: payment.status,
-    });
-  } catch (err) {
-    console.error("Checkout error:", err);
-    return NextResponse.json({ error: err.message }, { status: 500 });
-  }
-}
+//     if (!payment) {
+//       payment = new PaymentModel({
+//         _id: order.data.order_id,
+//         amount: order.data.order_amount,
+//         currency: order.data.order_currency,
+//         status: order.data.order_status,
+//         name: body.teamName ?? null,
+//         created_at: new Date(),
+//         order_id: order.data.order_id,
+//         payment_session_id: order.data.payment_session_id,
+//         user_id: body.userId ?? null,
+//       });
+//     }
+//     else {
+//       payment.status = order.data.order_status;
+//       payment.amount = order.data.order_amount;
+//       payment.currency = order.data.order_currency;
+//       payment.updated_at = new Date();
+//     }
+
+
+//     await payment.save();
+
+//     return NextResponse.json({
+//       id: order.data.order_id,
+//       amount: order.data.order_amount,
+//       currency: order.data.order_currency,
+//       status: payment.status,
+//     });
+//   } catch (err) {
+//     console.error("Checkout error:", err);
+//     return NextResponse.json({ error: err.message }, { status: 500 });
+//   }
+// }
